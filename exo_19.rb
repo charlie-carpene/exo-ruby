27158037b566fd80b0a89e511490528b9ced102d
Nathan Baliteau <nathanbaliteau@gmail.com>
2020-01-14 17:48:45 +0100

    add exo_19

---
+++ exo_19.rb
@@ -0,0 +1,12 @@
+mon_tableau=[]
+
+50.times do |i|
+  if i<9
+    mon_tableau.push("jean.dupont.0#{i+1}@email.fr")
+  else
+    mon_tableau.push("jean.dupont.#{i+1}@email.fr")
+  end
+  if i % 2 == 0
+    puts mon_tableau[i]
+  end
+end
