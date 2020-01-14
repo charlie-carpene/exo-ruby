a949a3a18ec7df6f4ebee15f237c8a37384f4477
Charlie Carpene <solunacisv@gmail.com>
2020-01-14 15:11:30 +0100

    exo 9

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Quel est ton prénom ?"
+print "> "
+user_firstname = gets.chomp
+puts "Quel est ton nom de famille ?"
+print "> "
+user_lastname = gets.chomp
+puts "Bonjour, #{user_firstname + " " + user_lastname} !"
